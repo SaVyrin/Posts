cb5a9376f847c00e112dab37ed942b433ab03c77
SaVyrin <sarajkinaa@gmail.com>
2022-07-14 16:39:08 +0300

    Add splash screen

---
+++ app/src/main/java/ru/surf/gallery/splash/SplashFragment.kt
@@ -0,0 +1,29 @@
+package ru.surf.gallery.splash
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import ru.surf.gallery.R
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+          findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+        }, 500) // TODO проверить сколько по тз
+    }
+}

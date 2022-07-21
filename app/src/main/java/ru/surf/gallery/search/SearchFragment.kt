fdd9de5c00ae7d630e35102f5b82a354a4fc8843
SaVyrin <sarajkinaa@gmail.com>
2022-07-21 15:51:07 +0300

    Replace search view with text edit

--- app/src/main/java/ru/surf/gallery/search/SearchFragment.kt
+++ app/src/main/java/ru/surf/gallery/search/SearchFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
 import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -59,18 +60,11 @@ class SearchFragment : Fragment() {
     }
 
     private fun setSearchViewTextListener() {
-        binding.searchView.setOnQueryTextListener(
-            object : SearchView.OnQueryTextListener {
-                override fun onQueryTextChange(newText: String): Boolean {
-                    viewModel.findMatchingPosts(newText)
-                    return false
-                }
-
-                override fun onQueryTextSubmit(query: String): Boolean {
-                    // task HERE
-                    return false
-                }
-            })
+        binding.searchViewEt.doOnTextChanged { newText, _, _, _ ->
+            newText?.let {
+                viewModel.findMatchingPosts(newText.toString())
+            }
+        }
     }
 
     private fun initRecyclerView() {
